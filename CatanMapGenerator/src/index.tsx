7465df6b9f11b8d61772a0cf4fc151f1ac1bd4d7
Dézsenyi Balázs <bz.dezsenyi@gmail.com>
2025-02-17 10:27:24 +0100

    vscodeInit

---
+++ CatanMapGenerator/src/index.tsx
@@ -0,0 +1,21 @@
+import { render } from 'preact';
+
+
+export function App() {
+	return (
+		<div>
+			AAA
+		</div>
+	);
+}
+
+function Resource(props) {
+	return (
+		<a href={props.href} target="_blank" class="resource">
+			<h2>{props.title}</h2>
+			<p>{props.description}</p>
+		</a>
+	);
+}
+
+render(<App />, document.getElementById('app'));
