859de867363730ea82ef866945411c164d68f170
Dézsenyi Balázs <bz.dezsenyi@gmail.com>
2025-02-17 14:30:50 +0100

    working no rules random map displayer

--- CatanMapGenerator/src/Containers/Map.tsx
+++ CatanMapGenerator/src/Containers/Map.tsx
@@ -1,15 +1,28 @@
 import { DesertHex } from "../Hexagons/DesertHex";
+import { ForestHex } from "../Hexagons/ForestHex"; 
+import { MountainHex } from "../Hexagons/MountainHex"; 
+import { FieldHex } from "../Hexagons/FieldHex";
+import { HillHex } from "../Hexagons/HillHex";
+import { PastureHex } from "../Hexagons/Pasture";
 import "./Map.less";
 
 type MapProps = {};
+
+const hexTypes = [DesertHex, ForestHex, MountainHex,FieldHex,HillHex,PastureHex]; 
+
+function getRandomHexType() {
+    const randomIndex = Math.floor(Math.random() * hexTypes.length);
+    return hexTypes[randomIndex];
+}
 
 export function Map({}: Readonly<MapProps>) {
     const hexagonPattern = [3, 4, 5, 4, 3]; 
 
     const hexagons = hexagonPattern.map((count, row) => {
-        const hexagonsInRow = Array.from({ length: count }, (_, col) => (
-            <DesertHex key={`${row}-${col}`} altText={`Hexagon ${row}-${col}`} />
-        ));
+        const hexagonsInRow = Array.from({ length: count }, (_, col) => {
+            const HexComponent = getRandomHexType();
+            return <HexComponent key={`${row}-${col}`} altText={`Hexagon ${row}-${col}`} />;
+        });
         return (
             <div key={row} className="hex-row">
                 {hexagonsInRow}
@@ -17,5 +30,5 @@ export function Map({}: Readonly<MapProps>) {
         );
     });
 
-    return <div class="Container"><div className="hex-grid">{hexagons}</div></div>;
+    return <div className="Container"><div className="hex-grid">{hexagons}</div></div>;
 }
