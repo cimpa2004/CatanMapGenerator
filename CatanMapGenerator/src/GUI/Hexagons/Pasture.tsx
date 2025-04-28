a54f0390b08d0b86956e083fd423235b47b2abcf
Dézsenyi Balázs <bz.dezsenyi@gmail.com>
2025-04-28 18:44:53 +0200

    new wheat

--- CatanMapGenerator/src/GUI/Hexagons/Pasture.tsx
+++ CatanMapGenerator/src/GUI/Hexagons/Pasture.tsx
@@ -1,9 +1,8 @@
 import pastureImage from "../../assets/newTiles/wheat.png";
-import nightImage from "../../assets/PetraTiles/placeholders/wheat.png"; //PetraTiles
+import nightImage from "../../assets/PetraTiles/wheat.png"; //PetraTiles
 import { getHexNumberImage } from "./GetHexNumberImage";
 import { HexProps } from "./HexProps";
 
-//should never display a value
 export function PastureHex({ value,altText,nightMode }: Readonly<HexProps>) {
     let image = pastureImage;
     if(nightMode===true){
